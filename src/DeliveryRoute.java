14b3a3dd2a978838c4d7f27cd11cffbe1ec62b41
whitewill1 <sceadugengan@gmail.com>
2017-06-07 13:59:49 +1200

    wrote a djikstra search for the deliveryRoute. cant find one yet as the routeservice doesnt support a function to get routes from a location

--- src/DeliveryRoute.java
+++ src/DeliveryRoute.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class DeliveryRoute {
 	private List<Route> deliveryRoute;
@@ -14,6 +16,7 @@ public class DeliveryRoute {
 	public DeliveryRoute(String origin){
 		this.origin = origin;
 		this.deliveryRoute = new ArrayList<Route>();
+		this.destination = origin; //goes nowhere yet
 	}
 
 	/**
@@ -50,5 +53,64 @@ public class DeliveryRoute {
 
 	public String getDestination() {
 		return destination;
+	}
+
+	/**
+	 * pseudo constructor to find a DeliveryRoute between origin and destination, will return that delivery route if it exists otherwise null
+	 * @param origin
+	 * @param destination
+	 * @param priority
+	 * @return
+	 */
+	public static DeliveryRoute findRoute(String origin, String destination, boolean priority){
+
+		PriorityQueue<DeliveryRoute> queue = new PriorityQueue<DeliveryRoute>(new RouteComparitor());
+
+		queue.add(new DeliveryRoute(origin));
+
+		List<Route> availableRoutes = new ArrayList<>();
+
+		DeliveryRoute currentRoute;
+
+		//TODO get pathfinding working
+
+		while(!(queue.peek().getOrigin().equals(origin) && queue.peek().getDestination().equals(destination)) && !queue.isEmpty()){ //keep polling the priorityQueue until the first route off is from origin to destination
+
+			//availableRoutes = RouteService.getRoutesFrom(currentRoute.destination);//TODO method i need doesnt exist yet
+			currentRoute = queue.poll();
+			DeliveryRoute tempRoute;
+
+			for(Route r: availableRoutes){
+				if(r.getTransportType().equals(Route.TransportType.AIR)){ //only add air routes if this is priority delivery
+					if (priority){
+						tempRoute = new DeliveryRoute(currentRoute);
+						tempRoute.addRoute(r);
+						queue.add(tempRoute);
+					}
+
+				} else {
+					tempRoute = new DeliveryRoute(currentRoute); //add all land and sea routes regardless
+					tempRoute.addRoute(r);
+					queue.add(tempRoute);
+				}
+			}
+
+			currentRoute = queue.poll();
+		}
+
+		return(queue.isEmpty()? null: queue.poll()); //if we ran out of routes to check return null, otherwise the first route in the queue should be the complete route
+	}
+
+	/**
+	 * Comparator for prioritizing deliveryRoutes by totalCost
+	 * @author whitewill1
+	 *
+	 */
+	private static class RouteComparitor implements Comparator<DeliveryRoute>{
+		@Override
+		public int compare(DeliveryRoute r1, DeliveryRoute r2) {
+			return (r1.getTotalPrice()>r2.getTotalPrice()? -1:1);//TODO, may have this the wrong way around, need to test it
+		}
+
 	}
 }
